b54cd9c0b2f85ad5684b5001dd138a34636e1781
Pgrz <p@AlphaCentauri>
2015-11-26 19:54:10 +0100

    Better make

--- source/main.c
+++ source/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    char out[10];
+    strcpy(out, "Output");
+
     #pragma omp parallel
     {
-        printf("Duuuupaaa\n");
+        printf("%s\n", out);
     }
     return 0;
 }
